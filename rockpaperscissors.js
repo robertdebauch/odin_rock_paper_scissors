31d8d7ac8348e6d78075ecc641a893c511fd3c22
robert_debauch <robert.debauch@gmail.com>
2025-06-19 12:25:08 +0500

    feat: add pseudocode plan for step 2

---
+++ rockpaperscissors.js
@@ -0,0 +1,9 @@
+// Step 2 pseudocode
+// A function is created for the computer's response
+// A random number is generated inside the function
+// The generated number is checked for conditions
+// If is compared with a predefined range of values
+// Each range corresponds to a 'string' value for the computer's response
+// If the generated number matches a certain range, it is outputed as 'return value'
+// The check if performed step-by-step until the comparison conditions are met.
+
